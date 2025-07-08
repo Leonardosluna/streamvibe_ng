6c6ca57ca785b2750bca9cd841b2e40ff3b20329
Leonardosluna <leonardoluna26@gmail.com>
2025-07-08 20:40:43 -0300

    primeira versão

---
+++ src/app/componentes/header/header.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+// importação da biblioteca componente do Angular.
+
+//Decorator, o component informa como o seu componente estoa estruturado. 
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.html',
+  styleUrl: './header.css'
+})
+export class Header {
+
+}// Exportando a classe Header.
