8eff4868aac9a7f46ca1af7a2f46f0b6df7f2e5e
Leonardosluna <leonardoluna26@gmail.com>
2025-07-10 19:52:40 -0300

    Atualização

--- src/app/componentes/filme/filme.ts
+++ src/app/componentes/filme/filme.ts
@@ -8,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class Filme {
 
+  filme1 = {
+    nome: "A Origem",
+    imagem: "/Aorigem.jpg",
+    sinopse: "Um grande sonhador que sonha dentro dos sonhos de quem ta sonhando"
+  }
+
+  filme2 = {
+    nome: "Batman: O Cavaleiro das Trevas",
+    imagem: "/batman.jpg",
+    sinopse: "Um maluco vestido de morcego desce a porrada num maluco vestido de palhaço"
+  }
+
+  filme3 = {
+    nome: "Interstelar",
+    imagem: "/Interstelar.jpg",
+    sinopse: "Um grupo de malucos entra num buraco de minhoca"
+  }
+
 }
