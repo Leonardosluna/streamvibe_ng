2f803f65649768d3342b537722657184a1e2770e
Leonardosluna <leonardoluna26@gmail.com>
2025-07-10 21:41:55 -0300

    Atualização 10.7.2025

--- src/app/componentes/filme/filme.ts
+++ src/app/componentes/filme/filme.ts
@@ -8,22 +8,27 @@ import { Component } from '@angular/core';
 })
 export class Filme {
 
+  alternarFavorito() {
+    this.filme1.favorito = !this.filme1.favorito
+  }
+
   filme1 = {
     nome: "A Origem",
     imagem: "/Aorigem.jpg",
-    sinopse: "Um grande sonhador que sonha dentro dos sonhos de quem ta sonhando"
+    sinopse: "Um grande sonhador que sonha dentro dos sonhos de quem ta sonhando",
+    favorito: false
   }
 
-  filme2 = {
-    nome: "Batman: O Cavaleiro das Trevas",
-    imagem: "/batman.jpg",
-    sinopse: "Um maluco vestido de morcego desce a porrada num maluco vestido de palhaço"
-  }
+  // filme2 = {
+  //   nome: "Batman: O Cavaleiro das Trevas",
+  //   imagem: "/batman.jpg",
+  //   sinopse: "Um maluco vestido de morcego desce a porrada num maluco vestido de palhaço"
+  // }
 
-  filme3 = {
-    nome: "Interstelar",
-    imagem: "/Interstelar.jpg",
-    sinopse: "Um grupo de malucos entra num buraco de minhoca"
-  }
+  // filme3 = {
+  //   nome: "Interstelar",
+  //   imagem: "/Interstelar.jpg",
+  //   sinopse: "Um grupo de malucos entra num buraco de minhoca"
+  // }
 
 }
